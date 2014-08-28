ad79ea2c1462c14e07c0c5c2fa1bd204246a5cda
Oliver Bridgman <oli.bridgman@gmail.com>
2014-08-28 23:08:46 +1200

    Fields and relationship for artist and release. many_many due to compilations and other releases I haven't yet thought of

--- mysite/code/dataobjects/Release.php
+++ mysite/code/dataobjects/Release.php
@@ -1,4 +1,28 @@
 <?php
 class Release extends DataObject{
 
+  private static $db = array(
+    'Title'           => 'Varchar(255)',
+    'ReleaseNo'       => 'Varchar(255)',
+    'ReleaseDate'     => 'Date',
+    'Content'         => 'HTMLText',
+    'BandcampURL'     => 'Varchar(255)',
+    'FeaturedRelease' => 'Boolean',
+    'URLSegment'      => 'Varchar(255)'
+  );
+
+  private static $belongs_many_many = array(
+    'Artist' => 'ArtistPage'
+  );
+
+  private static $summary_fields = array(
+    'Title',
+    'ReleaseNo'
+  );
+
+  public function getCMSFields() {
+    $fields = parent::getCMSFields();
+
+    $fields->removeByName('URLSegment');
+  }
 }
--- mysite/code/pages/ArtistPage.php
+++ mysite/code/pages/ArtistPage.php
@@ -1,13 +1,20 @@
 <?php
 class ArtistPage extends Page{
 
-  private static $db = array();
+  private static $db = array(
+    'FacebookURL' => 'varchar(255)',
+    'TwitterURL' => 'varchar(255)',
+    'BandcampURL' => 'varchar(255)',
+    'CartelURL' => 'varchar(255)'
+  );
 
   private static $has_one = array();
 
   private static $has_many = array();
 
-  private static $many_many = array();
+  private static $many_many = array(
+    'Releases' => 'Release'
+  );
 
   private static $belongs_many_many = array();
 
@@ -36,6 +43,27 @@ class ArtistPage extends Page{
   private static $can_be_root = true;
 
   private static $hide_ancestor = null;
+
+  function getCMSFields(){
+    $fields = parent::getCMSFields();
+
+    $gridFieldConfig = GridFieldConfig_RelationEditor::create()->addComponents(
+      new GridFieldDeleteAction('unlinkrelation'));
+      $releasesField = new GridField(
+        'Releases',
+        'Releases',
+        $this->Releases(),
+        $gridFieldConfig
+      );
+    $fields->addFieldToTab('Root.Releases', $releasesField);
+
+    $fields->addFieldToTab('Root.Main', new TextField('FacebookURL'), 'Content');
+    $fields->addFieldToTab('Root.Main', new TextField('TwitterURL'), 'Content');
+    $fields->addFieldToTab('Root.Main', new TextField('BandcampURL'), 'Content');
+    $fields->addFieldToTab('Root.Main', new TextField('CartelURL'), 'Content');
+          
+    return $fields;
+  }
   
 }
 
